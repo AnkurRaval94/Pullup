ff1112bba4ce87c91c8dd566143ba1155e8a7e01
AnkurRaval94 <ankurraval94@gmail.com>
2017-04-26 23:55:24 -0400

    Initial commit

---
+++ app/src/main/java/com/example/ankurraval94/pullup/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.ankurraval94.pullup;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void createAccount(View view) {
+        Intent intent = new Intent(getApplicationContext(), SignUp.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
